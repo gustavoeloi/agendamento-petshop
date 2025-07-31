1b4ed7940a7318f9dff539963a725aaec002f064
Gustavo Eloi <gustavooeloi@gmail.com>
2025-07-31 14:16:37 -0300

    config webpack, plugins, babel and packages

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// config dayjs
+import "./libs/dayjs";
+
+//CSS
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/layout.css";
+
+import dayjs from "dayjs";
